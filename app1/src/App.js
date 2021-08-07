f37bbf0bce877774e201dd2bee3ec23d70f244e3
Syedfarhan111 <syedfarhan5170@gmail.com>
2021-08-07 17:31:05 +0530

    56u5

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,45 +1,11 @@
-import logo from './logo.svg';                
-import './App.css';
-import {Courseinfo} from "./Component/Courseinfo"
+import logo from "./logo.svg";
+import "./App.css";
+import { Dictionary } from "./Component/Dictionary";
 
 function App() {
   return (
-    <div className="App">
-      <h1 style={{textAlign:"center" }}> <b> List of Institutions </b></h1>
-
-      <tr>
-      <td><Courseinfo Cname="Pdac" MobileNo="7706208868" cityNmae1="pune" instituteName="3rI technology " /></td>
-      <pre>   </pre>
-      <td><Courseinfo Cname="c-dac"  MobileNo="254698752" cityNmae="Mumbai" instituteName=" naresh it" /></td>
-      <pre>   </pre>
-      <td> <Courseinfo Cname=" Hacking" MobileNo="88526947" cityNmae=" Aurangabad" instituteName="itm collage" /></td>
-      <pre>   </pre>
-      </tr>
-      <pre>   </pre> <hr />
-   
-
-      <tr>
-      <td><Courseinfo Cname="PDAc" MobileNo="7706208868" cityNmae="pune" instituteName="3rI technology " /></td>
-      <pre>   </pre>
-      <td><Courseinfo Cname="c-dac"  MobileNo="254698752" cityNmae="Mumbai" instituteName=" naresh it" /></td>
-      <pre>   </pre>
-      <td> <Courseinfo Cname=" Hacking" MobileNo="88526947" cityNmae=" Aurangabad" instituteName="itm collage" /></td>
-      <pre>   </pre>
-      </tr>
-      <pre>   </pre> <hr />
-      
-
-
-      <tr>
-      <td><Courseinfo Cname="Pdac" MobileNo="7706208868" cityNmae="pune" instituteName="3rI technology " /></td>
-      <pre>   </pre>
-      <td><Courseinfo Cname="c-dac"  MobileNo="254698752" cityNmae="Mumbai" instituteName=" naresh it" /></td>
-      <pre>   </pre>
-      <td> <Courseinfo Cname=" Hacking" MobileNo="88526947" cityNmae=" Aurangabad" instituteName="itm collage" /></td>
-      <pre>   </pre>
-      </tr>
-      <pre>   </pre> <hr />
-      
+    <div>
+      <Dictionary />
     </div>
   );
 }
---
+++ app1/src/Component/Dictionary.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Dictionary = () => {
+    return (
+        <div>
+            <Navbar expand="lg" variant="light" bg="light">
+    <Container>
+      <Navbar.Brand href="#">Navbar</Navbar.Brand>
+    </Container>
+  </Navbar>
+
+
+            
+        </div>
+    );
+};
+
+export default Dictionary;
